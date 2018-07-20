d15b1a24d5e9893dc1ffc1a4022373f0639811e4
Dinos Kousidis <dinos.kousidis@cern.ch>
2018-07-20 15:32:28 +0200

    travis: addition of docker image build test
    Signed-off-by: Dinos Kousidis <dinos.kousidis@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -26,3 +26,4 @@ check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test && \
 sphinx-build -qnNW -b doctest docs docs/_build/doctest
+docker build -t reanahub/reana-workflow-engine-serial .
