4b94287ccf7a1697da832a1f5deb5f1f0f8fa2e0
Rokas Maciulaitis <rokas.maciulaitis@cern.ch>
2019-01-14 17:33:51 +0200

    tasks: decoding json values passed from WFC
    * Decoding json flag values passed from WFC.
      Connects reanahub/reana-workflow-controller/issues/167
    
    Signed-off-by: Rokas Maciulaitis <rokas.maciulaitis@cern.ch>

--- reana_workflow_engine_serial/tasks.py
+++ reana_workflow_engine_serial/tasks.py
@@ -11,6 +11,7 @@
 from __future__ import absolute_import, print_function
 
 import click
+import base64
 import json
 import logging
 import os
@@ -40,9 +41,12 @@ def escape_shell_arg(shell_arg):
 
     return "%s" % shell_arg.replace('"', '\\"')
 
+
 def load_json(ctx, param, value):
     """Callback function for click option"""
-    return json.loads(value)
+    value = str.encode(value[1:])
+    return json.loads(base64.standard_b64decode(value).decode())
+
 
 @click.command()
 @click.option('--workflow-uuid',
