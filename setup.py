512173c2647ab67c40196ffddeaa534f4b8a4aa3
Rokas Maciulaitis <rokas.maciulaitis@cern.ch>
2019-08-23 11:03:02 +0200

    setup: reana-commons upgrade

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup_requires = [
 
 install_requires = [
     'click>=7',
-    'reana-commons>=0.6.0.dev20190812,<0.7.0',
+    'reana-commons>=0.6.0.dev20190823,<0.7.0',
 ]
 
 packages = find_packages()
