6eea6b8389d5625e4ddfb8fb3ce98e0b7244df80
Adelina Lintuluoto <adelina.eleonora.lintuluoto@cern.ch>
2020-03-30 15:49:11 +0100

    proxy: change field from proxy to voms_proxy

--- reana_workflow_engine_serial/tasks.py
+++ reana_workflow_engine_serial/tasks.py
@@ -174,7 +174,7 @@ def run_step(step_number,
             kerberos=step.get('kerberos', False),
             unpacked_image=step.get('unpacked_image', False),
             kubernetes_uid=step.get('kubernetes_uid', None),
-            proxy=step.get('proxy', False))
+            voms_proxy=step.get('voms_proxy', False))
 
         job_spec_copy = dict(job_spec)
         job_spec_copy['cmd'] = sanitize_command(job_spec_copy['cmd'])
